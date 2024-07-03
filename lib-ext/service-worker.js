329ae9f6d6f8db8f083ecf06b7080eed4d6e310f
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-07-03 07:26:50 +0300

    Выбор в combobox

--- lib-ext/service-worker.js
+++ lib-ext/service-worker.js
@@ -24,11 +24,21 @@ chrome.action.onClicked.addListener((tab) => {
 });
 
 function fillTable(values) {
-    let ids = ["sys_code", "author", "supervisor", "critic", "title", "keywords", "annotation",
-	       "year1", "pages_count", "publishing", "ui-spec", "ui-kafv", "ui-vo"];
+    const ids = ["sys_code", "author", "supervisor", "critic", "title", "keywords", "annotation",
+		 "year1", "pages_count", "publishing", "ui-spec", "ui-kafv", "ui-vo"];
+    const opts = ["spec", "kafv", "vo"];
     for (let i = 0; i < 10/*ids.length*/; i++) {
 	let l = document.getElementById(ids[i]);
 	l.value = values[i];
-	l.dispatchEvent(new Event('change'));
     }
+    els = document.getElementsByClassName("custom-combobox-input");
+    for (let i = 0; i < 3; i++) {
+	els[i].value = values[10 + i];
+    }
+    let l = document.getElementById("spec");
+    l.options.item((values[10][4] == '3') ? 39 : 42).selected = true;
+    l = document.getElementById("kafv");
+    l.options.item(19).selected = true;
+    l = document.getElementById("vo");
+    l.options.item((values[10][4] == '3') ? 2 : 4).selected = true;
 }
