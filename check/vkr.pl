99919ac37d56a42319e8b174ca7b49aad1df23f4
Чаплыгин Александр <alex_chaplygin@mail.ru>
2025-05-13 17:10:11 +0300

    Обработка точки и точки с запятой для ненумерованных списков

--- check/vkr.pl
+++ check/vkr.pl
@@ -2,6 +2,32 @@ use strict;
 my $enumerate = 0;
 my $itemize = 0;
 my $line = 0;
+my %itemize_array;
+my %enumerate_array;
+my $num_items = 0;
+
+# Печать сообщения об ошибке
+# $msg - сообщение
+# $str - строка, где встретилась ошибка 
+# $num - номер строки
+sub print_msg {
+    my ($msg, $str, $num) = @_;
+    printf("%s\n", $msg);
+    printf("%i: %s\n", $num, $str);
+}
+
+# печать сообщения о двоеточии
+sub print_dot_comma {
+    my ($str, $num) = @_;
+    print_msg("Точка с запятой в конце:", $str, $num);
+}
+
+# печать сообщения о точке
+sub print_dot {
+    my ($str, $num) = @_;
+    print_msg("Точка в конце:", $str, $num);
+}
+
 while (<>) {
     $line++;
     if (m/begin\{enumerate/) {
@@ -10,20 +36,35 @@ while (<>) {
 	$enumerate = 0;
     } elsif (m/begin\{itemize/) {
 	$itemize = 1;
+	$num_items = 0;
     } elsif (m/end\{itemize/) {
 	$itemize = 0;
+	for (my $i = 0; $i < $num_items; $i++) {
+	    my $l = $itemize_array{$i};
+	    if ($l !~ m/\;$/ && $i < $num_items - 1) {
+		print_dot_comma($l, $line - ($num_items - $i));
+	    } elsif ($l !~ m/\.$/ && $i == $num_items - 1) {
+		print_dot($l, $line - ($num_items - $i));
+	    }
+	}
+	#print(%itemize_array);
     } else {
 	s/^\s+//;
 	s/\s+$//;
 	if ($enumerate && !m/\.$/) {
-	    printf("Точка в конце:\n");
-	    printf("%i: %s\n", $line, $_);
+	    print_dot($_, $line);
 	}
-#	if ($itemize && !m/\;$/) {
-#	    printf("Двоеточие в конце:\n");
-#	    printf("%i: %s\n", $line, $_);
-#	}
-	if ($enumerate && !m/^\\item\s+[А-Я]/ && !m/^\\item\s+[A-Z]/) {
+	if ($itemize) {
+	    if (m/\\item/) {
+		$itemize_array{$num_items} = $_;
+	    } else {
+		$itemize_array{$num_items} += $_;
+	    }
+	    if (m/\\item/) {
+		$num_items++;
+	    }
+	}
+	if ($enumerate && !m/^\\item\s+[А-Я]/) { # && !m/^\\item\s+[A-Z]/) {
 	    printf("С заглавной буквы:\n");
 	    printf("%i: %s\n", $line, $_);
 	}
